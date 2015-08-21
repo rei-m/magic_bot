d6523fe0115fbac1922e2802d1ddb2a06cd7b5f2
rei-m <star.oceans.rm@wj8.so-net.ne.jp>
2015-08-21 19:22:55 +0900

    add lgtm

--- lib/scripts.ex
+++ lib/scripts.ex
@@ -1,16 +1,27 @@
 defmodule Scripts do
   use Slack
 
-  def hear("hear?", message, slack) do
-    send_message("I can hear", message.channel, slack)
+  require HTTPoison
+  require Floki
+
+  def hear("lgtm", message, slack) do
+    HTTPoison.start
+    case URI.encode("http://www.lgtm.in/g") |> HTTPoison.get do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} -> body
+        |> Floki.find("#imageUrl")
+        |> Floki.attribute("value")
+        |> hd
+        |> send_message(message.channel, slack)
+      {_, _} -> nil
+    end
   end
 
   # Don't remove. This is default pattern.
   def hear(_, _, _) do
   end
 
-  def respond("respond?", message, slack) do
-     send_message("I can respond", message.channel, slack)
+  def respond("エリクサーほしい？", message, slack) do
+     send_message("エリクサーちょうだい！\nhttp://img.yaplog.jp/img/01/pc/2/5/2/25253/1/1354.jpg", message.channel, slack)
   end
 
   # Don't remove. This is default pattern.
